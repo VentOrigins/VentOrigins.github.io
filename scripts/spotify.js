4f2acb2ea30797d1499f09e42272fa1d7fa5a5d7
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-07-08 18:43:54 -0700

    Edited spotiy.js for login

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -7,58 +7,66 @@
 
 //When the Web App begins
 
-
+var accessToken = "";
+var state = "";
+var bear = "";
+var userID = "";
 
 
 $(document).ready(function() {
 	console.log("Document ready");
   // This function is anonymous, is executed immediately and 
   // the return value is assigned to QueryString!
-  var scopes = 'playlist-modify playlist-read-private playlist-modify-public playlist-modify-private user-read-private playlist-read-collaborative';
-	var my_client_id = 'f516a166c50d43dfae1800141104d748'
-	var redirect_uri = 'http://ventorigins.github.io'
-	var uri = 'https://accounts.spotify.com/authorize?' + 
-	  '&client_id=' + my_client_id +
-	  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-	  '&redirect_uri=' + encodeURIComponent(redirect_uri)
-	  + '&response_type=token&state=444'
-	window.location = uri;
-	var accessToken = "";
-	var state = "";
-	var bear = "";
-	var userID = "";
+   var vars = window.location.href.split("&");
+	console.log(vars);
+  for (var i=0;i<vars.length;i++) {
+	  var pair = vars[i].split("=");
+		if(i == 0) {
+			if(pair[1].indexOf("access_denied") < 0) {	
+	  		accessToken = pair[1];
+	  		console.log("Access Token = " + accessToken);	
+			}
+			else {
+				console.log("ACCESS DENIDED");
+			}
+		}
+		else if(i == 1) {
+			bear = pair[1];
+			console.log("Bear = " + bear);
+		}
+		else if(i == 3) {
+			state = pair[1];
+			console.log("State = " + state);
+		}
+	}
+	if(accessToken != "") {
+		$.ajax({
+			url: 'https://api.spotify.com/v1/me',
+			headers: {
+			   'Authorization': 'Bearer ' + accessToken
+			},
+			success: function(response) {
+			   findUserID(response);
+			}
+		});
+	}
+	else {
+		var scopes = 'playlist-modify playlist-read-private playlist-modify-public playlist-modify-private user-read-private playlist-read-collaborative';
+		var my_client_id = 'f516a166c50d43dfae1800141104d748'
+		var redirect_uri = 'http://ventorigins.github.io'
+		var uri = 'https://accounts.spotify.com/authorize?' + 
+		  '&client_id=' + my_client_id +
+		  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+		  '&redirect_uri=' + encodeURIComponent(redirect_uri)
+		  + '&response_type=token&state=444'
+		window.location = uri;
+	}
+  
+
   // FOR DESIGN
-  var vars = window.location.href.split("&");
-console.log(vars);
-for (var i=0;i<vars.length;i++) {
-  var pair = vars[i].split("=");
-	if(i == 0) {
-		if(pair[1].indexOf("access_denied") < 0) {	
-  		accessToken = pair[1];
-  		console.log("Access Token = " + accessToken);	
-		}
-		else {
-			console.log("ACCESS DENIDED");
-		}
-	}
-	else if(i == 1) {
-		bear = pair[1];
-		console.log("Bear = " + bear);
-	}
-	else if(i == 3) {
-		state = pair[1];
-		console.log("State = " + state);
-	}
-}	
-$.ajax({
-	url: 'https://api.spotify.com/v1/me',
-	headers: {
-	   'Authorization': 'Bearer ' + accessToken
-	},
-	success: function(response) {
-	   findUserID(response);
-}
-});
+
+	
+	
 });
 
 
