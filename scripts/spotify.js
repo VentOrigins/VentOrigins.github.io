ef063b741057b27942622960db7057009414c1cf
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-07-08 18:39:34 -0700

    Edited spotiy.js for login

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -7,30 +7,28 @@
 
 //When the Web App begins
 
-var scopes = 'playlist-modify playlist-read-private playlist-modify-public playlist-modify-private user-read-private playlist-read-collaborative';
-var my_client_id = 'f516a166c50d43dfae1800141104d748'
-var redirect_uri = 'http://ventorigins.github.io'
-var uri = 'https://accounts.spotify.com/authorize?' + 
-  '&client_id=' + my_client_id +
-  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  '&redirect_uri=' + encodeURIComponent(redirect_uri)
-  + '&response_type=token&state=444'
-window.location = uri;
-var accessToken = "";
-var state = "";
-var bear = "";
-var userID = "";
+
 
 
 $(document).ready(function() {
 	console.log("Document ready");
   // This function is anonymous, is executed immediately and 
   // the return value is assigned to QueryString!
-
+  var scopes = 'playlist-modify playlist-read-private playlist-modify-public playlist-modify-private user-read-private playlist-read-collaborative';
+	var my_client_id = 'f516a166c50d43dfae1800141104d748'
+	var redirect_uri = 'http://ventorigins.github.io'
+	var uri = 'https://accounts.spotify.com/authorize?' + 
+	  '&client_id=' + my_client_id +
+	  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+	  '&redirect_uri=' + encodeURIComponent(redirect_uri)
+	  + '&response_type=token&state=444'
+	window.location = uri;
+	var accessToken = "";
+	var state = "";
+	var bear = "";
+	var userID = "";
   // FOR DESIGN
-});
-
-var vars = window.location.href.split("&");
+  var vars = window.location.href.split("&");
 console.log(vars);
 for (var i=0;i<vars.length;i++) {
   var pair = vars[i].split("=");
@@ -51,9 +49,7 @@ for (var i=0;i<vars.length;i++) {
 		state = pair[1];
 		console.log("State = " + state);
 	}
-}
-
-
+}	
 $.ajax({
 	url: 'https://api.spotify.com/v1/me',
 	headers: {
@@ -63,6 +59,12 @@ $.ajax({
 	   findUserID(response);
 }
 });
+});
+
+
+
+
+
 /*  =============================================================================
 		From: Spotify.js when index.html starts up
 		To: playlist.js (displayPlaylist)
