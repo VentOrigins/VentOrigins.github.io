a65381762f309cc456d595643d2b5013ae381dc4
Adrian Mandee <Amandee@users.noreply.github.com>
2015-07-15 19:52:28 -0700

    edited to playlist startup

--- scripts/spotifyPlaylist.js
+++ scripts/spotifyPlaylist.js
@@ -8,7 +8,15 @@
 var vars = window.location.href.split("&");
 console.log(vars);
 if(vars.length < 2) {
-  console.log("ACCESS DENIED")
+    var scopes = 'playlist-modify playlist-read-private playlist-modify-public playlist-modify-private user-read-private';
+    var my_client_id = 'f516a166c50d43dfae1800141104d748'
+    var redirect_uri = 'http://ventorigins.github.io/playlist.html'
+    var uri = 'https://accounts.spotify.com/authorize?' + 
+      '&client_id=' + my_client_id +
+      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+      '&redirect_uri=' + encodeURIComponent(redirect_uri)
+      + '&response_type=token&state=444'
+    window.location = uri;
 }
 else {
   for (var i=0;i<vars.length;i++) {
