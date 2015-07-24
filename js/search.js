8d3a3ba37a8009b14ff0c8388d775dd4f1f70e7c
Randy Truong <rtruo001@ucr.edu>
2015-07-23 18:36:24 -0700

    Testing google API, added search bar and button

--- js/search.js
+++ js/search.js
@@ -1,55 +1,124 @@
+/*  =============================================================================
+    
+    Copyright © Vent Origins 
+    By Adrian Mandee and Randy Truong
+    ========================================================================== */
+
+/*  =============================================================================
+    Searches through youtube and soundcloud to return the list of tracks
+    From: OnSubmit of query-input
+    To: youtubeSearch and soundCloudSearch
+
+    @param        none
+    @return       none
+    ========================================================================== */
 function search() {
-  keyWordsearch();
+  // Removes defaults
+  event.preventDefault();
+  // Puts an overlay over the splash screen to display tracks
+  overlayTracks("#splash-screen");
 
-	event.preventDefault();
-	console.log("Hello");
-	var query = $("#query-input").val();
-	SC.initialize({
-  client_id: 'bd791d329c430374438075140d3d3163'
-	});
-	// find all sounds of buskers licensed under 'creative commons share alike'
-
-
-	
-	var page_size = 30;
-	SC.get('/tracks', { q: query, limit: page_size, linked_partitioning: 1}, function(tracks) {
-	  overlayTracks("#splash-screen");
-	  displayTracksOnOverlay(tracks);
-	  console.log(tracks.collection.length);
-	  console.log(tracks.collection[0].title);
-	  console.log(tracks.collection[0].permalink_url);
-	  console.log(tracks.collection[0].user.username);
-	  console.log(tracks.collection[0].user.permalink_url);
-	  console.log(tracks.next_href);
-	  var track_url = tracks.collection[0].permalink_url;
-		SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
-		  console.log(oEmbed);
-		});
-
-	});
-
+  // Begins Youtube and SoundCloud search
+  youTubeSearch();
+  soundCloudSearch();
 }
 
+/*  =============================================================================
+    Does the youtube search part, sets up the API key and makes the request
+    From: search()
+    To: youTubeMakeRequest()
 
-function keyWordsearch(){
-            console.log("asd");
-            gapi.client.setApiKey('AIzaSyDJckImr2TEKtJepdGvM0rFU8vlTw-Pufw');
-            gapi.client.load('youtube', 'v3', function() {
-                    makeRequest();
-            });
-    }
-    function makeRequest() {
-            console.log("test2")
-            var q = $('#query-input').val();
-            var request = gapi.client.youtube.search.list({
-                       q: q,
-                    part: 'snippet'                        
-            });
-            request.execute(function(response) {
-                    var str = JSON.stringify(response.result);
-                    $('#splash-screen').html('<pre>' + str + '</pre>');
-            });
-    }
+    @param        none
+    @return       none
+    ========================================================================== */
+function youTubeSearch() {
+  console.log("Beginning search through Youtube");
+  gapi.client.setApiKey('AIzaSyDJckImr2TEKtJepdGvM0rFU8vlTw-Pufw');
+  gapi.client.load('youtube', 'v3', function() {
+    youTubeMakeRequest();
+  });
+}
+    
+/*  =============================================================================
+    Makes the request of the query value inputted from the query-input
+    From: youTubeSearch()
+    To: displayYoutubeOnOverlay()
+
+    @param        none
+    @return       none
+    ========================================================================== */
+function youTubeMakeRequest() {
+  console.log("Making request to Google's API");
+  var q = $('#query-input').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'                        
+  });
+  request.execute(function(videos) {
+    displayYoutubeOnOverlay(videos);
+  });
+}
+
+/*  =============================================================================
+    Does the soundcloud search part, sets up the API key and makes the request
+    From: search()
+    To: soundCloudMakeRequest()
+
+    @param        none
+    @return       none
+    ========================================================================== */
+function soundCloudSearch() {
+  console.log("Beginning search through SoundCloud");
+  SC.initialize({
+    client_id: 'bd791d329c430374438075140d3d3163'
+  });
+  soundCloudMakeRequest();
+}
+
+/*  =============================================================================
+    Makes the request of the query value inputted from the query-input
+    From: soundCloudSearch()
+    To: displaySoundCloudOnOverlay()
+
+    @param        none
+    @return       none
+    ========================================================================== */
+function soundCloudMakeRequest() {
+  var q = $("#query-input").val();
+  var page_size = 30;
+
+  SC.get(
+    '/tracks', 
+    { 
+      q: query,
+      limit: page_size, 
+      linked_partitioning: 1
+    }, 
+    function(tracks) {
+      displaySoundCloudOnOverlay();
+      console.log(tracks.collection.length);
+      console.log(tracks.collection[0].title);
+      console.log(tracks.collection[0].uri);
+      console.log(tracks.collection[0].user.username);
+      console.log(tracks.collection[0].user.permalink_url);
+      console.log(tracks.next_href);
+      var track_url = tracks.collection[0].permalink_url;
+      SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+        console.log(oEmbed);
+    });
+  });
+}
+
+function displayYoutubeOnOverlay(videos) {
+  var str = JSON.stringify(videos.result);
+  $('#splash-screen').html('<pre>' + str + '</pre>');
+}
+
+function displaySoundCloudOnOverlay(tracks) {
+  var str = JSON.stringify(tracks.result);
+  $('#splash-screen').html('<pre>' + str + '</pre>');
+}
+
 
 
 $(document).ready(function() {
