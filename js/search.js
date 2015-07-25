975bb90488e923e75fea8f0d618c37d053eadee0
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-07-24 19:16:57 -0700

    Editing json results

--- js/search.js
+++ js/search.js
@@ -137,7 +137,7 @@ function soundCloudMakeRequest() {
     ========================================================================== */
 function displayYoutubeOnOverlay(videos) {
   var str = JSON.stringify(videos.result);
-  console.log("Youtube" + str[0]);
+  console.log("Youtube" + vidoes.result[0]);
   $('#youTubeTracks').append('<pre>' + str + '</pre>');
 }
 
