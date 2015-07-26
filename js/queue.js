3d7968c970ebd6a3e7ea27ac79da23117e1fee01
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-07-26 02:59:25 -0700

    Editing queue.js

--- js/queue.js
+++ js/queue.js
@@ -1,5 +1,5 @@
-function addToQueue(this) {
-	var href = $(this).attr('href');
+function addToQueue(buttonClicked) {
+	var href = $(buttonClicked).attr('href');
 	var videoID = href.substring(31,href.length);
 	$("#queues").append("<li><i class='fa fa-youtube'></i> <div class='queue-text'>" + videoID + "</div></li>")
 }
