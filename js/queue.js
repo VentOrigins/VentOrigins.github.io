da108c36a14b55e091a71cb6f05a95a68b68b00e
Randy Truong <rtruo001@ucr.edu>
2015-07-26 23:23:50 -0700

    Added Soundcloud to queue

--- js/queue.js
+++ js/queue.js
@@ -1,4 +1,12 @@
-function addToQueue(buttonClicked) {
-	var videoID = $(buttonClicked).attr('id');
-	$("#queues").append("<li><i class='fa fa-youtube'></i> <div class='queue-text'>" + videoID + "</div></li>")
+function addToQueue(buttonClicked, youtubeOrSoundCloud) {
+	
+  var videoID = $(buttonClicked).attr('id');
+
+  // Appends the correct logo to the queue bar depending if youtube or soundclick was added
+  if(youtubeOrSoundCloud == 'youtube') {
+    $("#queues").append("<li><i class='fa fa-youtube'></i> <div class='queue-text'>" + videoID + "</div></li>")
+  }
+  else if (youtubeOrSoundCloud == 'soundcloud') {
+    $("#queues").append("<li><i class='fa fa-soundcloud'></i> <div class='queue-text'>" + videoID + "</div></li>")
+  }
 }
--- js/search.js
+++ js/search.js
@@ -139,12 +139,11 @@ function displayYoutubeOnOverlay(videos) {
   for(var i = 0; i < videos.result.items.length; i++) {
     // VIDEO ID TO THE URL LINK
     videoID = videos.result.items[i].id.videoId;
-    button = "<button id='" + videoID + "' class='addButton' onclick='addToQueue(this)'> + </button> "
+    button = "<button id='" + videoID + "' class='addButton' onclick='addToQueue(this, 'youtube')'> + </button> "
     title = "<div class='youTubeTracksText'>" + "<a href='" + "http://www.youtube.com/watch?v=" + videoID + "'>" + videos.result.items[i].snippet.title + "</a>"+ "</div>";
     thumbnail = "<div class='image-thumbnail'><img src='" + videos.result.items[i].snippet.thumbnails.default.url + "' alt='playlist-image'></div>";
     str = button + title + thumbnail;
     $('#youTubeTracks').append("<div class='youTubeTracksRow'>" + str + "</div>");
-
   }
   checkSize();
 }
@@ -168,7 +167,7 @@ function displaySoundCloudOnOverlay(tracks) {
   console.log(tracks)
   for (var i = 0; i < tracks.collection.length; ++i) {  
     uri = tracks.collection[i].uri;
-    button = "<button id='" + uri + "' class='addButton' onclick='addToQueue(this)'> + </button>"
+    button = "<button id='" + uri + "' class='addButton' onclick='addToQueue(this, 'soundcloud')'> + </button>"
 
     title = "<div class='soundCloudTracksText'>" + "<a href='" + uri + "'>" + tracks.collection[i].title + "</a>" + "</div>";
     username = tracks.collection[i].user.username;
