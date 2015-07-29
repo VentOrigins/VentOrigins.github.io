2f9e45d1f9fcb23a101525b76bc6e1453d6e1267
Randy Truong <rtruo001@ucr.edu>
2015-07-28 21:27:21 -0700

    Moved SCPlayer above volume

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -4,7 +4,7 @@ function displaySoundCloudPlayer() {
 	console.log(localStorage.getItem('currPlaying'));
 	checkSize();
 	var uri = encodeURIComponent(localStorage.getItem('currPlaying'));
-	$("#soundCloudPlayer").append("<iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=" + uri + "'width='100%' height='200' scrolling='no' frameborder='no'></iframe>");
+	$("#soundCloudPlayer").prepend("<iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=" + uri + "'width='100%' height='200' scrolling='no' frameborder='no'></iframe>");
 	
 	var widgetIframe = document.getElementById('sc-widget'),
 	widget = SC.Widget(widgetIframe);
