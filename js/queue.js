547458f78f1d8a1a96568ae1014d58ce3ba6738f
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-08-04 17:42:07 -0700

    Previous Queue

--- js/queue.js
+++ js/queue.js
@@ -166,9 +166,8 @@ function removeQueue(position) {
   var pos = position.toString();
   $("#li"+pos).remove();
   localStorage.removeItem(pos);
-  console.log($("#queues").text());
-  console.log($("#queues").length);
-  
+  console.log($.trim($("#queues").text()).length;
+
   if(localStorage.getItem('currPosition') == pos) {
     nextQueue();
   }
