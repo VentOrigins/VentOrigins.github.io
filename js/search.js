1eeeed3cb2cabefddccf79d1c4316198975be687
Randy Truong <rtruo001@ucr.edu>
2015-08-06 00:05:51 -0700

    Working with Youtube Overlay

--- js/search.js
+++ js/search.js
@@ -157,11 +157,13 @@ function displayYoutubeOnOverlay(videos, allVideoContent) {
       
       //Seperating URI and title to parse
       thumbnail = "<div class='image-thumbnail'><img src='" + videos.result.items[i].snippet.thumbnails.default.url + "' alt='playlist-image'></div>";
-      text = "<div class='youTubeTracksText'>" + "<button id='" + videoID + "/|" + videos.result.items[i].snippet.title + "' class='addButton' onclick='addYoutubeToQueue(this)'>" + videos.result.items[i].snippet.title + "</button> <br>" + ' ' + allVideoContent.result.items[j].contentDetails.duration + "<br>" + ' ' + "<a href='https://www.youtube.com/channel/" + videos.result.items[i].snippet.channelId + "' target='_blank'> <i class='fa fa-desktop fa-lg fa-align-center'></i> </a> </div>";
+      text = "<div class='youTubeTracksText'>" + "<button id='" + videoID + "/|" + videos.result.items[i].snippet.title + "' class='addButton' onclick='addYoutubeToQueue(this)'>" + videos.result.items[i].snippet.title + "</button> <br>" + ' ' + allVideoContent.result.items[j].contentDetails.duration + "<br>";
       // videoDuration = "<div class='youTubeDuration'>" + allVideoContent.result.items[i].contentDetails.duration + "</div>";
       // channel = "<div class='youTubeChannelTitle'>" + videos.result.items[i].snippet.channelTitle + "</div>";
+      
+      links = "<a href='https://www.youtube.com/channel/" + videos.result.items[i].snippet.channelId + "' target='_blank'> <i class='fa fa-youtube-square fa-lg fa-align-center'></i> </a> </div>"
       // str = thumbnail + title + videoDuration + channel;
-      str = thumbnail + text;
+      str = thumbnail + text + links;
 
       // <i class='fa fa-television'></i>
       // <i class="fa fa-youtube fa-2x fa-align-center"></i>
