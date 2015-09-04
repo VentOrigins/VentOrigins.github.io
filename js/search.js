4be7efbbcc24cb8ad29dda66901408cae03a5246
Randy Truong <rtruo001@ucr.edu>
2015-09-04 01:47:39 -0700

    Edit overlay into one box

--- js/search.js
+++ js/search.js
@@ -294,6 +294,7 @@ function displayBothYoutubeAndSoundCloudOnOverlay(videos, allVideosContent, trac
     return;
   }
 
+  console.log('DisplayLength: ' + getGreaterLength(videos.result.items.length, tracks.collection.length));
   var displayLength = getGreaterLength(videos.result.items.length, tracks.collection.length);
   console.log('DisplayLength: ' + displayLength);
   for (var i = 0; i < displayLength; ++i) {
