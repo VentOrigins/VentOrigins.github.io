8bc27d7c518028bb750bc3cfae9206a1bc8572ee
Randy Truong <rtruo001@ucr.edu>
2015-09-04 01:49:03 -0700

    Edit overlay into one box

--- js/search.js
+++ js/search.js
@@ -295,7 +295,7 @@ function displayBothYoutubeAndSoundCloudOnOverlay(videos, allVideosContent, trac
   }
 
   console.log('DisplayLength: ' + getGreaterLength(videos.result.items.length, tracks.collection.length));
-  var displayLength = getGreaterLength(videos.result.items.length, tracks.collection.length);
+  var displayLength = getGreaterLength(finalVideos.result.items.length, finalTracks.collection.length);
   console.log('DisplayLength: ' + displayLength);
   for (var i = 0; i < displayLength; ++i) {
     displayYoutubeOnOverlay(i, finalVideos, finalAllVideoCOntent);
