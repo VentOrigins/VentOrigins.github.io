690222ed4907a4280b45ea033840601e96c8f843
Randy Truong <rtruo001@ucr.edu>
2016-02-02 16:55:35 -0800

    Testing

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   //Makes the width of the id's below to be the size of the screen minus the nav bar
   var nonNavScreenSize = screen.width - document.getElementById('nav').offsetWidth;
   $('#search-box').width(nonNavScreenSize);
-  $('#search-box').height(screen.height;
+  $('#search-box').height(screen.height);
   $('#track-list table').width(nonNavScreenSize);
   document.getElementById("search-box").style.left = document.getElementById('nav').offsetWidth + 'px';
   document.getElementById("splash-track-list").style.left = document.getElementById('nav').offsetWidth + 'px';
