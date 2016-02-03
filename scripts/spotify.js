7f146deb2e6c51fedbd9500d76981b68e9da83f4
Randy Truong <rtruo001@ucr.edu>
2016-02-02 16:47:01 -0800

    Minor changes

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
   // NOTE: If I had a chance to redo this, I would adjust the sizes depending on the screen changes
   //Makes the width of the id's below to be the size of the screen minus the nav bar
   var nonNavScreenSize = $(window).width() - document.getElementById('nav').offsetWidth;
-  $('#search-box').width(nonNavScreenSize);
+  // $('#search-box').width(nonNavScreenSize);
   $('#search-box').height($(window).height());
   $('#track-list table').width(nonNavScreenSize);
   document.getElementById("search-box").style.left = document.getElementById('nav').offsetWidth + 'px';
